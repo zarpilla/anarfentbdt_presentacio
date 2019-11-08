237f9ad6b88d6d3955b99df4717164dcde40b479
jordi-webcoop <jordi@webcoop.cat>
2019-11-08 13:51:30 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Anar Fent"/>
   </div>
 </template>
 
@@ -17,12 +16,26 @@ export default {
 </script>
 
 <style>
+@font-face {
+    font-family: 'Pine Forest Personal Use';
+    src: url('./assets/PineForestPersonalUse.woff2') format('woff2'),
+        url('./assets/PineForestPersonalUse.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+body{
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Pine Forest Personal Use', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,182 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <full-page :options="options">      
+      <div class="section">
+        <div class="title">          
+          QUè fEm 
+          <br>amb El NOstrE
+          <br>tEmps?
+          <br><br><br><br><br>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title2">
+          FEm util
+          <br>El NOstrE
+          <br>tEmps!
+          <br><br><br><br><br>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title2">
+          ANar FENt
+          <br><br>
+          BaNC dEL tEmps dEl RipOllès
+          <br><br>
+          www.aNarfENtbdt.Cat
+          <br><br><br><br><br>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title">
+          QUe és UN BaNC dEL tEmps
+          <ul class="ul">
+            <li>Xarxa de sUpOrt mUtú</li>
+            <li>Intercanvi serveis, coneixements, tasques</li>
+            <li>Sense diners</li>
+          </ul>
+          <br><br><br><br><br>
+        </div>        
+      </div>      
+      <div class="section">
+        <div class="title title2">
+          MONEda = TEmps
+        </div>        
+        <br><br><br><br>
+      </div>
+      <div class="section">
+        <div class="title title2">
+          Valors
+          <ul class="ul">
+            <li>integració social</li>
+            <li>relacions intergeneracionals i interculturals</li>
+            <li>relacions veïnals</li>
+            <li>autoestima</li>
+            <li>coneixença, igualtat, confiança i cooperació</li>
+            <li>conciliació de la vida personal, familiar i laboral</li>
+          </ul>
+          <br><br><br><br><br>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title">
+          Com neix el banc del temps del Ripollès
+          <ul class="ul">
+            <li>XES RipollEs</li>            
+          </ul>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title2">
+          Qui pot formar part del Banc
+          <ul class="ul">
+            <li>Persones majors de 18 anys</li>
+            <li>Entitats</li>
+            <li>Col·lectius</li>
+          </ul>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title2">
+          Com formar part del Banc
+          <ul class="ul">
+            <li>Formulari de <a href="https://docs.google.com/forms/d/1kyGJJOlIzL1ftFsfc8FigTECDBRI2murhsnUDvfVk-E" target="_blank">preinscripció</a></li>
+            <li>Entrevista personal</li>
+            <li>Aplicació web <a href="https://www.timeoverflow.org/" target="_blank">TimeOverflow</a></li>
+          </ul>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title">
+          TIMEOVERFLOW
+          <ul class="ul">
+            <li>Membres</li>            
+            <li>Ofertes</li>
+            <li>demandes</li>            
+          </ul>
+        </div>        
+      </div>         
+      <div class="section">
+        <div class="title title2">
+          Com hi poden participar les entitats
+          <ul class="ul">
+            <li>Ofertes i demandes</li>
+            <li>Compte solidari</li>            
+          </ul>
+        </div>        
+      </div>      
+      <div class="section">
+        <div class="title title2">
+          Tallers
+          <ul class="ul">
+            <li>Oferta o demanda Grupal</li>
+            <li>Promoguts des del banc</li>
+            <li>Promoguts des de les persones membres</li>
+          </ul>
+          <br><br><br><br>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title">
+          Com realitzar intercanvis
+          <ul class="ul">
+            <li>Aplicació web <a href="https://www.timeoverflow.org/" target="_blank">TimeOverflow</a></li>
+            <li>Membres, Ofertes i demandes</li>
+            <li>Contacte directe entre membres</li>
+            <li>Transferència de Temps</li>
+          </ul>
+          <br><br><br><br>
+        </div>        
+      </div>      
+      <div class="section">
+        <div class="title title2">
+          Quins intercanvis puc fer
+          <ul class="ul3">
+          <li>art i artesania</li>
+          <li>assessorament i orientació general</li>
+          <li>compres i comerç</li>
+          <li>cuina</li>
+          <li>cultura</li>
+          <li>cura d’animals</li>
+          <li>ensenyament</li>
+          <li>esports i animació</li>
+          <li>idiomes</li>
+          <li>informàtica i altres tecnologies</li>
+          <li>lleure i entreteniment</li>
+          <li>obra i construcció</li>
+          <li>salut i cura de persones</li>
+          <li>servei tècnic</li>
+          <li>serveis a la comunitat</li>
+          <li>teràpies alternatives</li>
+          <li>transport</li>
+          <li>treballs de jardineria</li>
+          <li>treballs de la llar</li>
+          </ul>
+          <br><br><br><br>
+        </div>        
+      </div>
+      <div class="section">
+        <div class="title title2">
+          Conducta
+          <ul class="ul">
+            <li>Respecte</li>
+            <li>Privacitat de dades</li>            
+          </ul>
+        </div>        
+      </div>      
+      <div class="section">
+        <div class="title title">
+          MOltEs GräCiEs!!!
+          <br><br>
+          ANar FENt
+          <br>
+          BaNC dEL tEmps dEl RipOllès
+          <br>
+          www.aNarfENtbdt.Cat
+          <br><br><br><br>
+        </div>        
+      </div>      
+    </full-page>    
   </div>
 </template>
 
@@ -35,24 +185,48 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {  
+    return {
+      options: {
+        scrollOverflow: false,
+        licenseKey: 'z',
+        navigation: true,
+        menu: '#menu',
+        sectionsColor: ['#ffffff', '#2C3E5D', '#2492AB','#ffffff', '#2C3E5D', '#2492AB','#ffffff', '#2C3E5D', '#2492AB','#ffffff', '#2C3E5D', '#2492AB','#ffffff', '#2C3E5D', '#2492AB','#ffffff', '#2C3E5D', '#2492AB'],
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.title {
+  font-size: 100px;
+  color:#111;
+  padding-top:200px
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+.title2 {
+  color:#fff;
 }
 a {
   color: #42b983;
+}
+ul{
+font-size: 50px;
+}
+ul li{
+  padding-bottom: 30px;
+}
+ul.ul2{
+  columns: 2;
+  -webkit-columns: 2;
+  -moz-columns: 2;
+}
+ul.ul3{
+  columns: 3;
+  -webkit-columns: 3;
+  -moz-columns: 3;
 }
 </style>
